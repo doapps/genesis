d0d9a0e96191b9b8af88c0acb87cc0af7b309be3
Juan C <vonzo55@gmail.com>
2016-08-03 19:55:36 -0500

    official tree sha

--- client/lib/api-handler/index.js
+++ client/lib/api-handler/index.js
@@ -4,9 +4,9 @@ const debug = require( 'debug' )( 'app:lib:api-handler' );
 
 // Github
 const githubApiPath = 'https://api.github.com';
-const rootSHA = 'a23caa93b11ca99f1ee1ccaacdba5f119333998b';
+const rootSHA = '2c2a75a5ee5a7385ae1128c99e7e3b2c07d08c65';
 const githubUser = 'doapps';
-const githubRepo = 'genesis';
+const githubRepo = 'dspp';
 
 const getTreePath = () =>
   `/repos/${ githubUser }/${ githubRepo }/git/trees/${ rootSHA }?recursive=true`;
