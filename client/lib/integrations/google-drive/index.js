3925e1fbbfff4a412a406100ed00c4ab0ffdfd39
Juan C <vonzo55@gmail.com>
2016-08-08 23:40:17 -0500

    meanwhile

--- client/lib/integrations/google-drive/index.js
+++ client/lib/integrations/google-drive/index.js
@@ -1,66 +1,20 @@
 const debug = require( 'debug' )( 'app:lib:integrations:google-drive' );
 
-const GDRIVE_CLIENT_ID = '1004924447672-ib5vtaiimi6h1sqfhq6b6o34iiheb8p8.apps.googleusercontent.com';
+// const GDRIVE_CLIENT_ID = '1004924447672-ib5vtaiimi6h1sqfhq6b6o34iiheb8p8.apps.googleusercontent.com';
+const GDRIVE_CLIENT_ID = '494857266143-s5miocne763456shvqr6nh5q8i4olqin.apps.googleusercontent.com';
 const GDRIVE_COOKIE_POLICY = 'single_host_origin';
-const GDRIVE_SCOPE = 'https://www.googleapis.com/auth/drive.file';
+const GDRIVE_SCOPE = 'https://www.googleapis.com/auth/drive';
+// const GDRIVE_SCOPE = 'https://www.googleapis.com/auth/drive.file';
+
+// Google App Script Variables
+const SCRIPT_URL_EXECUTION = 'https://script.googleapis.com';
+const SCRIPT_ID = 'MmfSf4AzQaez_YS5SScyV4uV-tPu4-D5Y';
+const SCRIPT_PATH_EXECUTION = `v1/scripts/${ scriptId }:run`;
+const FUNCTION_NAME = 'buildProject';
+
 
 let auth2;
 let element = null;
-
-const metadataFileTable = {
-  audio: 'application/vnd.google-apps.audio',
-  document: 'application/vnd.google-apps.document',
-  drawing: 'application/vnd.google-apps.drawing',
-  file: 'application/vnd.google-apps.file',
-  folder: 'application/vnd.google-apps.folder',
-  form: 'application/vnd.google-apps.form',
-  fusiontable: 'application/vnd.google-apps.fusiontable',
-  map: 'application/vnd.google-apps.map',
-  photo: 'application/vnd.google-apps.photo',
-  presentation: 'application/vnd.google-apps.presentation',
-  script: 'application/vnd.google-apps.script',
-  sites: 'application/vnd.google-apps.sites',
-  spreadsheet: 'application/vnd.google-apps.spreadsheet',
-  unknown: 'application/vnd.google-apps.unknown',
-  video: 'application/vnd.google-apps.video'
-};
-
-function requestWrapper( fn ) {
-  window.gapi.client.load( 'drive', 'v3', fn );
-}
-
-function getFolderMetadata( folderName ) {
-  return {
-    name: folderName,
-    mimeType: metadataFileTable.folder
-  };
-}
-
-function getFileMetadata( folderId, fileName ) {
-  return {
-    name: fileName,
-    parents: [ folderId ],
-    mimeType: metadataFileTable.document
-  };
-}
-
-function createFileHandler( fileMetadata ) {
-  const request = window.gapi.client.drive.files.create( {
-    fields: 'id',
-    resource: fileMetadata
-  } );
-
-  return request;
-}
-
-function listFileHandler() {
-  const request = window.gapi.client.drive.files.list( {
-    pageSize: 10,
-    fields: 'nextPageToken, files(id, name)'
-  } );
-
-  return request;
-}
 
 export const prepareAuth = cb => {
   window.gapi.load( 'auth2', () => {
@@ -89,46 +43,74 @@ export const prepareAuth = cb => {
   } );
 };
 
-export const createFolder = ( data, cb ) => {
-  requestWrapper( () => {
-    const { folderName } = data;
-    const folderMetadata = getFolderMetadata( folderName );
-    const request = createFileHandler( folderMetadata );
+export const runScriptBuilder = ( data, cb ) => {
+  const request = {
+    function: FUNCTION_NAME,
+    parameters: data
+  };
 
-    request.execute( resp => {
-      if ( resp.error ) {
-        cb( resp.error, null );
-        return;
-      }
+  const operation = window.gapi.request( {
+    root: SCRIPT_URL_EXECUTION,
+    path: SCRIPT_PATH_EXECUTION,
+    method: 'POST',
+    body: request
+  } );
 
-      cb( null, resp );
-    } );
+  operation.execute( resp => {
+    debug( 'resp', resp );
+
+    if ( resp.error && resp.error.status ) {
+      debug( 'Error calling API: ', res.error );
+      cb( res.error, null );
+    } else if ( resp.error ) {
+      debug( 'Script error:', resp.error.details[ 0 ] );
+      cb( res.error, null );
+    } else {
+      const payload = resp.response.result;
+
+      debug( 'API Executed: ', payload );
+      cb( null, payload );
+    }
   } );
 };
 
-export const createFile = ( data, cb ) => {
-  requestWrapper( () => {
-    const { folderId, fileName } = data;
-    const fileMetadata = getFileMetadata( folderId, fileName );
-    const request = createFileHandler( fileMetadata );
+export const executeScript = folderId => {
+  debug('------>', folderId);
+  // const folderId = '0BxTy39Zuq5lLanZpZDFDRkdFZ0k';
+  const request = {
+    'function': 'getFiles',
+    'parameters': folderId,
+     // "devMode": true,
+  };
 
-    request.execute( resp => {
-      if ( resp.error ) {
-        cb( resp.error, null );
-        return;
+  const op = gapi.client.request({
+    'root': 'https://script.googleapis.com',
+    'path': 'v1/scripts/' + scriptId + ':run',
+    'method': 'POST',
+    'body': request
+  });
+
+  op.execute(function(resp) {
+    console.log('resp', resp);
+    if (resp.error && resp.error.status) {
+      debug('Error calling API:');
+    } else if (resp.error) {
+      var error = resp.error.details[0];
+      debug('Script error message: ' + error.errorMessage);
+
+      if (error.scriptStackTraceElements) {
+        debug('Script error stacktrace:');
+        for (let i = 0; i < error.scriptStackTraceElements.length; i++) {
+          var trace = error.scriptStackTraceElements[i];
+          debug('\t' + trace.function + ':' + trace.lineNumber);
+        }
       }
+    } else {
+      var fileList = resp.response.result;
 
-      cb( null, resp );
-    } );
-  } );
+      fileList.forEach(function(file) {
+        debug(file.id + '\t' + file.name);
+      });
+    }
+  });
 };
-
-export const listFiles = cb => {
-  requestWrapper( () => {
-    const request = listFileHandler();
-
-    request.execute( resp => {
-      cb( resp );
-    } );
-  } );
-}
--- client/lib/integrations/trello/index.jsx
+++ client/lib/integrations/trello/index.jsx
@@ -17,10 +17,54 @@ const TrelloIntegration = React.createClass( {
         title="Trello"
         iconClass="fa fa-trello"
         description="for scrum"
-        isConnected
+        isConnected={ false }
         onClick={ this.connect } />
     );
   }
 } );
 
 export default TrelloIntegration;
+
+/*
+import React from 'react';
+
+import ConnectionCard from 'components/connection-card';
+import {executeScript} from 'lib/integrations/google-drive';
+
+const debug = require( 'debug' )( 'app:lib:integrations:trello' );
+
+const TrelloIntegration = React.createClass( {
+  displayName: 'TrelloIntegration',
+
+  getInitialState() {
+    return {
+      folderId: ''
+    }
+  },
+
+  connect() {
+    debug( 'connect trello' );
+    const {folderId} = this.state;
+    debug('folderId', folderId);
+    executeScript(folderId);
+  },
+
+  changeInput(event) {
+    this.setState({folderId: event.target.value});
+  },
+
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={ this.changeInput }/>
+        <button onClick={ this.connect }>
+        Click
+        </button>
+      </div>
+    );
+  }
+} );
+
+export default TrelloIntegration;
+
+ */
